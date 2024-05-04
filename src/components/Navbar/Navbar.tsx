87921684f57e0a95ccb19467501b600153b422db
notRaihan <justraihan@proton.me>
2024-05-04 19:27:03 +0700

    POS-10 fix: responsive design

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -7,9 +7,15 @@ const Navbar = () => {
         <h2 className={styles.navbar__brand}>Covid ID</h2>
       </div>
       <ul className={styles.navbar__list}>
-        <li className={styles.navbar__item}>About</li>
-        <li className={styles.navbar__item}>Indonesia</li>
-        <li className={styles.navbar__item}>Province</li>
+        <li className={styles.navbar__item}>
+          <a href="#">About</a>
+        </li>
+        <li className={styles.navbar__item}>
+          <a href="#">Indonesia</a>
+        </li>
+        <li className={styles.navbar__item}>
+          <a href="#">Province</a>
+        </li>
       </ul>
     </nav>
   );
