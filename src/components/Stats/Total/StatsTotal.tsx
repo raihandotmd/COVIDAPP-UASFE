22088dc19a17762812e84c9ae2febcf6abb4d52a
notRaihan <justraihan@proton.me>
2024-05-04 15:05:00 +0700

    POS-3 feat: StatsTotal Component

---
+++ src/components/Stats/Total/StatsTotal.tsx
@@ -0,0 +1,22 @@
+import styles from "./StatsTotal.module.css";
+import StatsCard from "../Card/StatsCard";
+
+const StatsTotal = () => {
+  return (
+    <div className={styles.StatsTotal}>
+      <div className={styles.StatsTotal__header}>
+        <h1 className={styles.StatsTotal__title}>Indonesia</h1>
+        <p className={styles.StatsTotal__subtitle}>
+          Covid Data Based on Indonesia.
+        </p>
+      </div>
+      <div className={styles.StatsTotal__body}>
+        <StatsCard status="Confirmed" stats={608.646} />
+        <StatsCard status="Recovered" colorStats="sec-color" stats={90.023} />
+        <StatsCard status="Death" colorStats="danger-color" stats={5.023} />
+      </div>
+    </div>
+  );
+};
+
+export default StatsTotal;
