e57b1e16a7633beb9e377fe2f657cdafb8791c8f
raihandotmd <justraihan@proton.me>
2024-07-10 06:16:31 +0700

    fix: responsive page and add icons

--- src/components/Stats/Total/StatsSituation.tsx
+++ src/components/Stats/Total/StatsSituation.tsx
@@ -4,8 +4,16 @@ import { SectionGlobalProps } from "../../../utils/types.ts";
 import { nanoid } from "nanoid";
 import { formatNumber } from "../../../utils/helpers.ts";
 import Section from "../../ui/Section/Section.tsx";
+import { PiFaceMask, PiSkull } from "react-icons/pi";
+import { BiHappyBeaming } from "react-icons/bi";
 
 type typeStatus = "confirmed" | "recovered" | "death";
+const size = 75;
+const icons = {
+  confirmed: <PiFaceMask size={size} />,
+  recovered: <BiHappyBeaming size={size} color={`var(--sec-color)`} />,
+  death: <PiSkull size={size} color={`var(--danger-color)`} />,
+};
 function checkStatus(status: typeStatus) {
   switch (status) {
     case "confirmed":
@@ -19,6 +27,19 @@ function checkStatus(status: typeStatus) {
   }
 }
 
+function checkIconsStatus(status: typeStatus) {
+  switch (status) {
+    case "confirmed":
+      return icons.confirmed;
+    case "recovered":
+      return icons.recovered;
+    case "death":
+      return icons.death;
+    default:
+      return icons.recovered;
+  }
+}
+
 const StatsSituation = ({ globalStats }: SectionGlobalProps) => {
   return (
     <Section
@@ -26,13 +47,14 @@ const StatsSituation = ({ globalStats }: SectionGlobalProps) => {
       subtitle="Data Covid Berdasarkan Global"
       bgColor="bglight"
     >
-      <div className={styles.StatsTotal__body}>
+      <div className={styles.StatsSituation__body}>
         {globalStats?.global.map((data) => (
           <span key={nanoid(5)}>
             <StatsCard
               status={data.status}
               colorStats={checkStatus(data.status as typeStatus)}
               stats={formatNumber(data.total)}
+              icon={checkIconsStatus(data.status as typeStatus)}
             />
           </span>
         ))}
